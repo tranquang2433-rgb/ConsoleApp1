803818beae4bbf2228c2ba8b47998237857f89d1
tranquang2433-rgb <tranquang2433@gmail.com>
2025-11-17 13:14:20 +0700

    Add project files.

---
+++ ConsoleApp1/CThiSinh.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum KieuKetQua { Dat,Khongdat}
+    public class CThiSinh
+    {
+        protected string m_MaTS, m_HoTen;
+        public string MaTS {
+            get { return m_MaTS; }
+            set { m_MaTS = value; }
+        }
+        public string HoTen
+        {
+            get { return m_HoTen; }
+            set { m_HoTen = value; }
+        }
+        public CThiSinh()
+        {
+            m_MaTS = "";
+            m_HoTen = "";
+        }
+        public CThiSinh(string ma,string ten)
+        {
+            m_MaTS = ma;
+            m_HoTen = ten;
+        }
+        public virtual double TongDiem()
+        {
+            return 0;
+        }
+        public virtual KieuKetQua KetQua()
+        {
+            return KieuKetQua.Khongdat;
+        }
+        public virtual bool LaKhoiA()
+        {
+            return false;
+        }
+        public virtual bool LaKhoiNangKhieu()
+        {
+            return false;
+        }
+    }
+    public class CKhoiA : CThiSinh
+    {
+        protected double m_DiemToan, m_DiemLy, m_DiemHoa;
+        public double DiemToan
+        {
+            get { return m_DiemToan; }
+            set
+            {
+                if (value < 0)
+                    m_DiemToan = 0;
+                else
+                    m_DiemToan = value;
+            }
+
+        }    
+        public double DiemLy
+        {
+            get { return m_DiemLy; }
+            set
+            {
+                if (value < 0)
+                    m_DiemLy = 0;
+                else
+                    m_DiemLy = value;
+            }
+        }
+        public double DiemHoa
+        {
+            get { return m_DiemHoa; }
+            set
+            {
+                if (value < 0)
+                    m_DiemHoa = 0;
+                else
+                    m_DiemHoa = value;
+            }
+        }
+        public CKhoiA() { }
+        public CKhoiA(string ma,string ten,double toan,double ly,double hoa)
+        {
+            m_MaTS = ma;
+            m_HoTen = ten;
+            m_DiemToan = toan;
+            m_DiemLy = ly;
+            m_DiemHoa = hoa;
+        }
+        public override double TongDiem()
+        {
+            return m_DiemToan+m_DiemLy+m_DiemHoa;
+        }
+        public override KieuKetQua KetQua()
+        {
+            if (TongDiem() >= 18.5)
+            {
+                return KieuKetQua.Dat;
+            }
+            return KieuKetQua.Khongdat;
+        }
+        public override bool LaKhoiA()
+        {
+            return true;
+        }
+        public override bool LaKhoiNangKhieu()
+        {
+            return false;   
+        }
+    }
+    class CKhoiNangKhieu : CThiSinh
+    {
+        protected double m_DiemToan, m_DiemNangKhieu;
+        public double DiemToan
+        {
+            get { return m_DiemToan; }
+            set
+            {
+                if (value < 0)
+                    m_DiemToan = 0;
+                else
+                    m_DiemToan = value;
+            }
+        }
+        public double DiemNangKhieu
+        {
+            get { return m_DiemNangKhieu; }
+            set
+            {
+                if (value < 0)
+                    m_DiemNangKhieu = 0;
+                else
+                    m_DiemNangKhieu = value;
+            }
+        }
+        public CKhoiNangKhieu() { }
+        public CKhoiNangKhieu(string ma,string ten,double toan,double nangkhieu)
+        {
+            m_MaTS = ma;
+            m_HoTen = ten;
+            m_DiemNangKhieu=nangkhieu;
+            m_DiemToan = toan;
+        }
+        public override double TongDiem()
+        {
+            return m_DiemToan + (m_DiemNangKhieu*2.0);
+        }
+        public override KieuKetQua KetQua()
+        {
+            if (TongDiem() >= 17)
+            {
+                return KieuKetQua.Dat;
+            }
+            return KieuKetQua.Khongdat;
+        }
+        public override bool LaKhoiA()
+        {
+            return false;
+        }
+        public override bool LaKhoiNangKhieu()
+        {
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static List<CThiSinh> NhapDSThiSinh()
+        {
+            List<CThiSinh> danhsach = new List<CThiSinh>();
+            Console.WriteLine("===Nhap danh sach thi sinh===");
+            Console.WriteLine("Nhap x de thoat");
+            while (true)
+            {
+                Console.Write("Nhap x de thoat hoac nhap enter de tiep tuc.");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "x")
+                {
+                    break;
+                }
+                Console.WriteLine("NHap thong tin hoc sinh.");
+                Console.Write("Nhap ma hoc sinh.");
+                string MaTS = Console.ReadLine();
+                Console.Write("NHap ho ten hoc sinh.");
+                string HoTen = Console.ReadLine();
+                Console.WriteLine("Chon khoi thi:");
+                Console.WriteLine("(1). Khoi A(Toan, Ly, Hoa)");
+                Console.WriteLine("(2). Khoi nang khieu(Toan, Nang khieu)");
+                Console.WriteLine("Hay chon 1 hoac 2");
+                string chonKhoi = Console.ReadLine();
+                try
+                {
+                    switch (chonKhoi)
+                    {
+                        case "1":
+                            Console.Write("Nhap diem mon toan");
+                            double DiemToan = double.Parse(Console.ReadLine());
+                            Console.Write("Nhap diem mon ly");
+                            double DiemLy = double.Parse(Console.ReadLine());
+                            Console.Write("Nhap diem mon Hoa");
+                            double DiemHoa = double.Parse(Console.ReadLine());
+                            CKhoiA thiSinhA = new CKhoiA(MaTS, HoTen, DiemToan, DiemLy, DiemHoa);
+                            if (thiSinhA.TongDiem() >= 18.5)
+                            {
+                                danhsach.Add(thiSinhA);
+                                Console.WriteLine("Them thanh cong.");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Diem du yeu cau. Them khong thanh cong.");
+                            }
+                            break;
+                        case "2":
+                            Console.Write("Nhap diem mon toan");
+                            double Diemtoan = double.Parse(Console.ReadLine());
+                            Console.Write("Nhap diem nang khieu");
+                            double diemNangKhieu = double.Parse(Console.ReadLine());
+                            CKhoiNangKhieu thiSinhB = new CKhoiNangKhieu(MaTS, HoTen, Diemtoan, diemNangKhieu);
+                            if (thiSinhB.TongDiem() >= 17)
+                            {
+                                danhsach.Add(thiSinhB);
+                                Console.WriteLine("Them thanh cong.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Diem du yeu cau. Them khong thanh cong.");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("❌ Lựa chọn không hợp lệ! Vui lòng chọn lại.");
+                            continue;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("❌ Lỗi: Điểm phải là số! Vui lòng nhập lại thí sinh này.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"❌ Lỗi: {ex.Message}");
+                }
+
+
+
+            }
+            //static int GetSoThiSinhKhoiNangKhieu(List<CThiSinh> ds)
+            //{
+
+            //}
+            return danhsach;
+        }
+    }
+}
